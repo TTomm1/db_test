469e91386170ac3f237dc5ad679c6ad579cd6688
TTomm1 <tudor.toma@gmail.com>
2017-02-09 13:50:32 +0100

    Update test2.py

---
+++ notebooks/Users/tudor.toma@quby.com/test_dir/test2.py
@@ -0,0 +1,32 @@
+# Databricks notebook source
+import sklearn as sl
+
+# COMMAND ----------
+
+#i edit outside db notebook
+#i edit in vim
+
+# COMMAND ----------
+
+1+1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is a markdown
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ssh
+
+# COMMAND ----------
+
+# MAGIC %run /Users/tudor.toma@quby.com/test_dir/tt2
+
+# COMMAND ----------
+
+message("bla bla")
+
+# COMMAND ----------
+
